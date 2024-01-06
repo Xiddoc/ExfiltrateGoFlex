02b029f35d30704184469b5d28f1266ea3dcd920
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2024-01-06 15:31:57 +0200

    Added debug mode for testing

--- flexfiltrate.py
+++ flexfiltrate.py
@@ -14,37 +14,45 @@ cli = typer.Typer()
 
 # noinspection PyUnusedFunction
 @cli.command()
-def shell(ip: str) -> None:
+def shell(ip: str, debug: bool = False) -> None:
     try:
         InteractiveShell.interactive(ip)
     except Exception as exception:
+        if debug:
+            raise
         print(exception)
 
 
 # noinspection PyUnusedFunction
 @cli.command()
-def execute(ip: str, command: str) -> None:
+def execute(ip: str, command: str, debug: bool = False) -> None:
     try:
         print(CommandExecutor.execute(ip, command).decode())
     except Exception as exception:
+        if debug:
+            raise
         print(exception)
 
 
 # noinspection PyUnusedFunction
 @cli.command()
-def ls(ip: str, path: str) -> None:
+def ls(ip: str, path: str, debug: bool = False) -> None:
     try:
         print("\n".join(ListFiles(ip).execute(path)))
     except Exception as exception:
+        if debug:
+            raise
         print(exception)
 
 
 # noinspection PyUnusedFunction
 @cli.command()
-def pull(ip: str, path: str) -> None:
+def pull(ip: str, path: str, debug: bool = False) -> None:
     try:
         ExfiltrateFiles(ip).execute(path)
     except Exception as exception:
+        if debug:
+            raise
         print(exception)
 
 
