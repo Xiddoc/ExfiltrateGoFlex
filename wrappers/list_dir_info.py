73dfd9918793a68e16ee90357a0edc103be66e31
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2024-01-06 08:55:56 +0200

    Better pattern matching for file name

--- wrappers/list_dir_info.py
+++ wrappers/list_dir_info.py
@@ -1,3 +1,4 @@
+import re
 from dataclasses import dataclass
 from typing import List
 
@@ -5,7 +6,7 @@ from goflex_shell.command_executor import CommandExecutor
 from wrappers.base_wrapper import BaseCommand
 
 LS_OUTPUT_PREFIX = 'total'
-
+LS_FILENAME_REGEX = re.compile(r'"(.*?)"')
 
 @dataclass
 class PathInfo:
@@ -16,7 +17,7 @@ class PathInfo:
 class ListPathInfo(BaseCommand):
 
     def execute(self, path: str) -> List[PathInfo]:
-        files = CommandExecutor.execute(self.ip, f'/bin/ls -l "{path}"').decode().splitlines()
+        files = CommandExecutor.execute(self.ip, f'/bin/ls --quote-name -l "{path}"').decode().splitlines()
 
         remove_header = files[1:] if files[0].startswith(LS_OUTPUT_PREFIX) else files
 
@@ -27,6 +28,6 @@ class ListPathInfo(BaseCommand):
     @staticmethod
     def _get_path_info_from_entry(entry: str) -> PathInfo:
         is_dir = entry.startswith('d')
-        file_name = entry.split(' ')[-1]
+        file_name = LS_FILENAME_REGEX.search(entry).group(1)
 
         return PathInfo(is_dir, file_name)
